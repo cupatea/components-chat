f28b72ccf9150f125167918858a852429610ae88
vladyslav <vladyslav@hey.com>
2021-12-19 02:14:18 +0200

    Add authorization helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  helper_method :current_user
+
+  def current_user
+    Current.user
+  end
+
+  def set_current_user
+    # finds user with session data and stores it if present
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user!
+    # allows only logged in user
+    redirect_to new_session_path, alert: 'You must be signed in' if current_user.nil?
+  end
 end
