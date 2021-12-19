9721493d85780da0458359cc36db851fced1828f
vladyslav <vladyslav@hey.com>
2021-12-19 02:03:05 +0200

    Fix configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,9 +56,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -69,4 +66,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
